128d5e497a43f810ca0c7e4621dcaf96c0e645d8
AKASH KULAL <akashkulal59@gmail.com>
2022-11-21 18:26:06 +0530

    completed with posts

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
 
 class FrontendController extends Controller
 {
@@ -13,13 +15,27 @@ class FrontendController extends Controller
     }
 
     //view category post
-    public function viewCategoryPost($category_slug)
+    public function viewCategoryPost(string $category_slug)
     {
         $category=Category::where('slug',$category_slug)->where('status','0')->first();
         if($category)
         {
-            $post=Post::where('category_id',$category->id)->where('status','0')->get();
-            return view('Frontend.Post.index');
+            $post=Post::where('category_id',$category->id)->where('status','0')->paginate(2);
+            return view('Frontend.Post.index',compact('post','category'));
+        }
+        else{
+            return redirect('/');
+        }
+    }
+
+    //view post slug
+    public function viewPost(string $category_slug,string $post_slug)
+    {
+        $category=Category::where('slug',$category_slug)->where('status','0')->first();
+        if($category)
+        {
+            $post=Post::where('category_id',$category->id)->where('slug',$post_slug)->where('status','0')->first();
+            return view('Frontend.Post.view',compact('post'));
         }
         else{
             return redirect('/');
